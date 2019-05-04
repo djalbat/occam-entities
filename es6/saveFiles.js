7be97b2428eeb5c51003bdadb5f49dbe37f50ba8
James Smith <james.smith@openmathematics.org>
2019-05-04 12:33:02 +0100

    Put the saveFiles() function back to what it was.

--- es6/saveFiles.js
+++ es6/saveFiles.js
@@ -1,9 +1,15 @@
 'use strict';
 
-function saveFiles(projectsDirectoryPath, files, callback) {
+const open = require('occam-open-cli');
+
+const { Files } = open;
+
+function saveFiles(projectsDirectoryPath, json, callback) {
+  const files = Files.fromJSON(json);
+
   files.save(projectsDirectoryPath);
 
-  const success = true; ///
+  const success = true;
 
   callback(success);
 }
