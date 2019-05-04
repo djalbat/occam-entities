86a62c404511b4919a10fb0973d23a4e04c0de21
James Smith <james.smith@openmathematics.org>
2019-05-04 13:02:06 +0100

    Altered the return value of the loadProjects() function.

--- es6/loadProjects.js
+++ es6/loadProjects.js
@@ -6,9 +6,10 @@ const { Projects } = open;
 
 function loadProjects(projectsDirectoryPath, doNotLoadHiddenFilesAndDirectories, callback) {
   const allowOnlyRecognisedFiles = false, ///
-        projects = Projects.fromProjectsDirectoryPath(projectsDirectoryPath, allowOnlyRecognisedFiles, doNotLoadHiddenFilesAndDirectories);
+        projects = Projects.fromProjectsDirectoryPath(projectsDirectoryPath, allowOnlyRecognisedFiles, doNotLoadHiddenFilesAndDirectories),
+		    json = projects.toJSON();
 
-  callback(projects);
+  callback(json);
 }
 
 module.exports = loadProjects;
