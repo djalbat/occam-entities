52170a416a5f31b894b3fdd83aa575141d52cd13
James Smith <james.smith@openmathematics.org>
2020-05-22 09:45:40 +0100

    Corrected the index file. Removed the import funtionality.

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  importProject: require('./lib/importProject'),
-  saveFiles: require('./lib/saveFiles'),
-  saveFile: require('./lib/saveFile'),
-  loadFile: require('./lib/loadFile'),
-  loadFiles: require('./lib/loadFiles'),
-  loadProjects: require('./lib/loadProjects'),
-  moveProjectEntries: require('./lib/moveProjectEntries'),
-  removeProjectEntries: require('./lib/removeProjectEntries')
+  saveFiles: require("./lib/saveFiles"),
+  saveFile: require("./lib/saveFile"),
+  loadFile: require("./lib/loadFile"),
+  loadFiles: require("./lib/loadFiles"),
+  loadProjects: require("./lib/loadProjects"),
+  moveProjectEntries: require("./lib/moveProjectEntries"),
+  removeProjectEntries: require("./lib/removeProjectEntries")
 };
