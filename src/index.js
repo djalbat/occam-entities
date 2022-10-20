e85286e2c1ae464a2809b3208fa3c19e7cce11d6
James Smith <james.smith@djalbat.com>
2022-10-20 20:26:08 +0100

    Propagated.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@
 
 export { default as pathMapsUtilities } from "./utilities/pathMaps";
 export { default as httpUtilities } from "./utilities/http";
-export { default as saveFiles } from "./saveFiles";
-export { default as saveFile } from "./saveFile";
 export { default as loadFile } from "./loadFile";
+export { default as saveFile } from "./saveFile";
 export { default as loadFiles } from "./loadFiles";
+export { default as saveFiles } from "./saveFiles";
 export { default as loadProjects } from "./loadProjects";
 export { default as moveProjectEntries } from "./moveProjectEntries";
 export { default as removeProjectEntries } from "./removeProjectEntries";
