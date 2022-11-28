6b6ce179c1477e242ac99bdbb37bc953dfad93d9
James Smith <james.smith@djalbat.com>
2022-11-28 19:03:08 +0000

    Corrected the renaming.

--- src/renameProjectEntry.js
+++ src/renameProjectEntry.js
@@ -1,10 +1,12 @@
 "use strict";
 
+import { move as rename } from "fs-extra";
 import { arrayUtilities } from "necessary";
+import { pathUtilities, fileSystemUtilities } from "necessary";
 
-import { moveEntryOperation } from "./moveProjectEntries";
-
-const { last } = arrayUtilities;
+const { last } = arrayUtilities,
+      { concatenatePaths } = pathUtilities,
+      { checkEntryExists, isEntryDirectory } = fileSystemUtilities;
 
 export default function renameProjectEntry(projectsDirectoryPath, json, callback) {
   const { pathMaps } = json,
@@ -12,7 +14,7 @@ export default function renameProjectEntry(projectsDirectoryPath, json, callback
         pathMap = lastPathMap,  ///
         { sourceEntryPath, targetEntryPath } = pathMap;
 
-  moveEntryOperation(sourceEntryPath, targetEntryPath, projectsDirectoryPath, (sourceEntryPath, targetEntryPath) => {
+  renameEntryOperation(sourceEntryPath, targetEntryPath, projectsDirectoryPath, (sourceEntryPath, targetEntryPath) => {
     const entryMissing = (sourceEntryPath === null),
           entryUnmoved = (sourceEntryPath === targetEntryPath),
           sourceEntryPaths = [],
@@ -46,5 +48,96 @@ export default function renameProjectEntry(projectsDirectoryPath, json, callback
     };
 
     callback(json);
+  });
+}
+
+export function renameEntryOperation(sourceEntryPath, targetEntryPath, projectsDirectoryPath, callback) {
+  const absoluteSourceEntryPath = concatenatePaths(projectsDirectoryPath, sourceEntryPath),
+        entryDirectory = isEntryDirectory(absoluteSourceEntryPath);
+
+  entryDirectory ?
+    renameDirectoryOperation(sourceEntryPath, targetEntryPath, projectsDirectoryPath, callback) :
+      renameFileOperation(sourceEntryPath, targetEntryPath, projectsDirectoryPath, callback);
+}
+
+function renameFileOperation(sourceEntryPath, targetEntryPath, projectsDirectoryPath, callback) {
+  const sourceFilePath = sourceEntryPath,  ///
+        targetFilePath = targetEntryPath;  ///
+
+  if (sourceFilePath === targetFilePath) {
+    callback(sourceEntryPath, targetEntryPath);
+
+    return;
+  }
+
+  const absoluteSourceFilePath = concatenatePaths(projectsDirectoryPath, sourceFilePath),
+        sourceFileExists = checkEntryExists(absoluteSourceFilePath);
+
+  if (!sourceFileExists) {
+    sourceEntryPath = null;
+
+    callback(sourceEntryPath, targetEntryPath);
+
+    return;
+  }
+
+  const absoluteTargetFilePath = concatenatePaths(projectsDirectoryPath, targetFilePath),
+        targetFileExists = checkEntryExists(absoluteTargetFilePath);
+
+  if (targetFileExists) {
+    targetEntryPath = sourceEntryPath;  ///
+
+    callback(sourceEntryPath, targetEntryPath);
+
+    return;
+  }
+
+  rename(absoluteSourceFilePath, absoluteTargetFilePath, (error) => {
+    if (error) {
+      targetEntryPath = sourceEntryPath;  ///
+    }
+
+    callback(sourceEntryPath, targetEntryPath);
+  });
+}
+
+function renameDirectoryOperation(sourceEntryPath, targetEntryPath, projectsDirectoryPath, callback) {
+  const sourceDirectoryPath = sourceEntryPath, ///
+        targetDirectoryPath = targetEntryPath; //
+
+  if (sourceDirectoryPath === targetDirectoryPath) {
+    callback(sourceEntryPath, targetEntryPath);
+
+    return;
+  }
+
+  const absoluteSourceDirectoryPath = concatenatePaths(projectsDirectoryPath, sourceDirectoryPath),
+        sourceDirectoryExists = checkEntryExists(absoluteSourceDirectoryPath);
+
+  if (!sourceDirectoryExists) {
+    sourceEntryPath = null;
+
+    callback(sourceEntryPath, targetEntryPath);
+
+    return;
+  }
+
+  const absoluteTargetDirectoryPath = concatenatePaths(projectsDirectoryPath, targetDirectoryPath),
+        targetDirectoryExists = checkEntryExists(absoluteTargetDirectoryPath);
+
+  if (targetDirectoryExists) {
+    targetEntryPath = sourceEntryPath;  ///
+
+    callback(sourceEntryPath, targetEntryPath);
+
+    return;
+  }
+
+  rename(absoluteSourceDirectoryPath, absoluteTargetDirectoryPath, (error) => {
+    if (error) {
+      targetEntryPath = sourceEntryPath;  ///
+    }
+
+    callback(sourceEntryPath, targetEntryPath);
   });
 }
