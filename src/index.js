ba7f966e1fbdd42ed1420728465bf1859e103c6b
James Smith <james.smith@djalbat.com>
2023-10-12 18:31:00 +0100

    Exported the Directory class.

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ export { default as Project } from "./project";
 export { default as Release } from "./release";
 export { default as Projects } from "./projects";
 export { default as Releases } from "./releases";
+export { default as Directory } from "./directory";
 export { default as fileNames } from "./fileNames";
 export { default as Dependency } from "./dependency";
 export { default as Dependencies } from "./dependencies";
