3a39c9dc7d1bb4ad03a53eafcfad7297d8386cb8
James Smith <james.smith@djalbat.com>
2024-05-14 17:12:52 +0100

    Added the sanitiseContent() function to the content utilities.

--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,6 @@
 
 export const EMPTY_STRING = "";
 export const DOUBLE_SPACE = "  ";
+export const ESCAPED_AMPERSAND = "&amp;";
+export const ESCAPED_LESS_THAN = "&lt;";
+export const ESCAPED_GREATER_THAN = "&gt;";
--- src/utilities/content.js
+++ src/utilities/content.js
@@ -1,12 +1,22 @@
 "use strict";
 
-import { EMPTY_STRING, DOUBLE_SPACE } from "../constants";
+import { DOUBLE_SPACE, EMPTY_STRING, ESCAPED_AMPERSAND, ESCAPED_LESS_THAN, ESCAPED_GREATER_THAN } from "../constants";
+
+export function sanitiseContent(content) {
+  const sanitisedContent = content
+                             .replace(/&/g, ESCAPED_AMPERSAND)
+                             .replace(/</g, ESCAPED_LESS_THAN)
+                             .replace(/>/g, ESCAPED_GREATER_THAN);
+
+  return sanitisedContent;
+}
 
 export function trimDoubleQuotes(content) { return content.replace(/(^"|"$)/g, EMPTY_STRING); } ///
 
 export function convertContentTabsToWhitespace(content) { return content.replace(/\t/g, DOUBLE_SPACE); } ///
 
 export default {
+  sanitiseContent,
   trimDoubleQuotes,
   convertContentTabsToWhitespace
 };
